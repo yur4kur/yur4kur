bf1e71d24c2b09dc11901d05932889b3c8843f8f
Iuri Kurinnoi <yur4kur@icloud.com>
2022-11-08 16:31:46 +0300

    Added alert learning project

---
+++ Learning Alerts/Learning Alerts/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Learning Alerts
+//
+//  Created by Юрий Куринной on 08.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
