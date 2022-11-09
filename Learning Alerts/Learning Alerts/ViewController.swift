8f88ec5d790a0e94385ee981749c1ea5b3a709e3
Iurii Kurinnoi <yur4kur@icloud.com>
2022-11-09 16:25:02 +0300

    added alert

--- Learning Alerts/Learning Alerts/ViewController.swift
+++ Learning Alerts/Learning Alerts/ViewController.swift
@@ -14,6 +14,20 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func showAlertButton() {
+        let alert = UIAlertController(title: "Photo Access", message: "Do you allow to use your photos?", preferredStyle: .alert)
+        
+        let okAction = UIAlertAction(title: "Allow", style: .cancel)
+        alert.addAction(okAction)
+        
+        let cancelAction = UIAlertAction(title: "Don't allow", style: .default, handler: nil)
+        alert.addAction(cancelAction)
+        
+        present(alert, animated: true)
+    }
+    
+    @IBAction func showActionSheetButton() {
+    }
+    
 }
 
